354067bcfa3c74b516a4952bf3047aed3401fa19
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-03-11 13:56:33 +0100

    pong1

--- pong/script.js
+++ pong/script.js
@@ -1,1 +1,29 @@
+class Circle {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  draw(){
+    context.beginPath();
+    context.strokeStyle = "red";
+    context.fillStyle = "aliceblue";
+    context.lineWidth = "4";
+    context.arc(this.x,this.y,50,0,Math.PI*2);
+    context.closePath();
+    context.stroke();
+    context.fill();
+  }
+}
+
 const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let myCircle = new Circle(300,500);
+myCircle.draw();
+
+let otherCircle = new Circle(400,400)
+otherCircle.draw();
